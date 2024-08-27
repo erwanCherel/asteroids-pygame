8272803fc5bdfe8b1905ac6a1cb99ce323577c83
Erwan Cherel <erwancherel.pro@gmail.com>
2024-08-27 07:47:13 +0200

    add splitted asteroids

--- asteroid.py
+++ asteroid.py
@@ -13,3 +13,16 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        random_angle = random.uniform(20, 50)
+        angle_1 = self.velocity.rotate(random_angle)
+        angle_2 = self.velocity.rotate(-random_angle)
+        self.radius -= ASTEROID_MIN_RADIUS
+        asteroid_1 = Asteroid(self.position.x, self.position.y, self.radius)
+        asteroid_2 = Asteroid(self.position.x, self.position.y, self.radius)
+        asteroid_1.velocity = angle_1 * 1.2
+        asteroid_2.velocity = angle_2 * 1.2
